8e9254fb7d01aafb02dd6838231ce4cc3aa37a17
Dere Samad Babatunde <90105837+deresamad@users.noreply.github.com>
2025-06-03 04:17:57 -0400

    Add files via upload

---
+++ Deskcalculator.java
@@ -0,0 +1,22 @@
+run:
+Your order of computer desk:::
+Type of wood: Mahogany
+Length: 90.0 cm
+Width: 90.0 cm
+Number of drawers: 2
+Final cost: $460.0
+
+Your order of computer desk:::
+Type of wood: Oak
+Length: 50.0 cm
+Width: 45.0 cm
+Number of drawers: 3
+Final cost: $415.0
+
+Your order of computer desk:::
+Type of wood: Pine
+Length: 75.0 cm
+Width: 68.0 cm
+Number of drawers: 1
+Final cost: $280.0
+BUILD SUCCESSFUL (total time: 0 seconds)
